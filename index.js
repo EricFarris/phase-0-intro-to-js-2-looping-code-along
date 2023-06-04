27ccc58c0ae31f0c0c101e6b5002465b0541c9df
Eric Farris <efarris1993@gmail.com>
2023-06-04 17:05:00 -0400

    needs word

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards([names, eventType]){
+let messages = [];
+
+for (const name of names) {
+messages.push(`thank you, ${name}, for the wonderful ${eventType} gift!`)
+}
+return messages;
+}
+
+function countDown(start) {
+    for (let num = i; i >= 0; --i) {
+        console.log(num);
+    }
+}
